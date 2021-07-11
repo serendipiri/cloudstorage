c6e104234898b4076ea18c066f0878e909e2b407
serendipiri <seren.piri@gmail.com>
2021-07-11 14:51:08 +0300

    SecurityConfig created and started to implement login-signup

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationProviderService.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class AuthenticationProviderService implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
