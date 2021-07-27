077b8f720cdb7f87e8d2138ad4d0f1b51d8c1fd7
serendipiri <seren.piri@gmail.com>
2021-07-27 23:00:52 +0300

    larger files are still a problem

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/common/ErrorHandler.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/common/ErrorHandler.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.common;
 
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +13,7 @@ public class ErrorHandler {
     @Value("${spring.servlet.multipart.max-file-size}")
     private String maxFileSize;
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ExceptionHandler({MaxUploadSizeExceededException.class, FileSizeLimitExceededException.class})
     public String handleFileSizeLimitExceeded(MaxUploadSizeExceededException exc, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("activeTab", "files");
         redirectAttributes.addFlashAttribute("errorMsgFile",
