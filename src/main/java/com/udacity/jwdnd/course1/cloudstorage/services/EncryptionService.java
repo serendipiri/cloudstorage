20b1a95a52d9c4f83fed5a6fb192a2b4caefa475
serendipiri <seren.piri@gmail.com>
2021-11-12 11:43:29 +0300

    -

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
@@ -2,7 +2,6 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.*;
@@ -14,6 +13,7 @@ import java.util.Base64;
 
 @Service
 public class EncryptionService {
+
     private Logger logger = LoggerFactory.getLogger(EncryptionService.class);
 
     public String encryptValue(String data, String key) {
